da556ce687fce29ba906a20997be737074baaa20
Fraktalia <g.w.koscielniak@gmail.com>
2021-04-29 00:32:48 +0200

    Implement categories.

--- src/main/java/com/codecool/shop/dao/ProductCategoryDao.java
+++ src/main/java/com/codecool/shop/dao/ProductCategoryDao.java
@@ -8,6 +8,7 @@ public interface ProductCategoryDao {
 
     void add(ProductCategory category);
     ProductCategory find(int id);
+    ProductCategory find(String categoryName);
     void remove(int id);
 
     List<ProductCategory> getAll();
--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -25,5 +25,11 @@ public class ProductService{
         return productDao.getBy(category);
     }
 
+    public List<Product> getAllProducts() {
+        return productDao.getAll();
+    }
 
+    public ProductCategory getProductCategoryByName(String categoryName) {
+        return productCategoryDao.find(categoryName);
+    }
 }
