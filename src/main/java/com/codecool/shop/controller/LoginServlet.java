47a8481c10b3227c1c0912dab1029832347dc1b7
kasluk345 <kaszuba.lukasz@gmail.com>
2021-04-28 14:33:37 +0200

    retrive data form login servlet works

--- src/main/java/com/codecool/shop/controller/LoginServlet.java
+++ src/main/java/com/codecool/shop/controller/LoginServlet.java
@@ -1,6 +1,17 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.UserDaoMem;
+import com.codecool.shop.model.User;
 import com.codecool.shop.service.LoginService;
+import com.codecool.shop.service.ProductService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,18 +20,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "loginServlet", urlPatterns = {"/login"})
+@WebServlet(urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
     private LoginService loginService;
 
-    //Czy powinno rzucać Servlet Excepion?
-    @Override
-    public void init() {
-        loginService = new LoginService();
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        loginService.ha;
+        UserDao userDataStore = UserDaoMem.getInstance();
+        UserDao userDao = UserDaoMem.getInstance();
+        LoginService loginService = new LoginService(userDataStore);
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        engine.process("login.html", context, resp.getWriter());
     }
-}
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LoginService loginService = new LoginService();
+        loginService.handlePOSTLogin(req, resp);
+        doGet(req, resp);
+        }
+    }
---
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoMem.java
@@ -0,0 +1,45 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class UserDaoMem implements UserDao {
+    private List<User> data = new ArrayList<>();
+    private static UserDaoMem instance = null;
+
+
+    private UserDaoMem() {
+    }
+
+    public static UserDaoMem getInstance() {
+        if (instance == null) {
+            instance = new UserDaoMem();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(User user) {
+        user.setId(UUID.randomUUID().toString());
+        data.add(user);
+    }
+
+    @Override
+    public User find(int id) {
+        return data.stream().filter(t -> t.getId().equals(id)).findFirst().orElse(null);    }
+
+    @Override
+    public void remove(int id) {
+        data.remove(find(id));
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return data;
+    }
+}
